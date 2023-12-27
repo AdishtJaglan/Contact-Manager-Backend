858a859d8a9730562f836bdb5e498eb829c5aef8
AdishtJaglan <adisht7524@gmail.com>
2023-12-27 23:44:16 +0530

    CRUD op to create new contact added

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,42 +1,53 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
+
 //@desc Get all contacts
 //@routes GET /api/contacts
 //@access public
-const getContacts = asyncHandler((req, res) => {
-    res.status(200).json({ message: "Get all contacts." });
+const getContacts = asyncHandler(async (req, res) => {
+    const contacts = await Contact.find()
+    res.status(200).json(contacts);
 });
 
 //@desc Create New contacts
 //@routes POST /api/contacts
 //@access public
-const createContact = asyncHandler((req, res) => {
+const createContact = asyncHandler(async (req, res) => {
     console.log("the request body is: ", req.body);
     const { name, email, phone } = req.body;
+
     if (!name || !email || !phone) {
         res.status(400);
         throw new Error("All fields are mandatory!")
     }
-    res.status(201).json({ message: "Create contact." });
+
+    const contact = await Contact.create({
+        name,
+        email,
+        phone,
+    });
+
+    res.status(201).json(contact);
 });
 
 //@desc Get contact
 //@routes GET /api/contacts/:id
 //@access public
-const getContact = asyncHandler((req, res) => {
+const getContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Get contact for ${req.params.id}` });
 });
 
 //@desc Update contacts
 //@routes PUT /api/contacts/:id
 //@access public
-const updateContact = asyncHandler((req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update contact for ${req.params.id}` });
 });
 
 //@desc Delete contacts
 //@routes POST /api/contacts/:id
 //@access public
-const deleteContact = asyncHandler((req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Deleted contact for ${req.params.id}` });
 });
 
