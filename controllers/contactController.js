4e7b4bd6860eb900621f1b4bc71af84a6f822fd9
AdishtJaglan <adisht7524@gmail.com>
2023-12-28 00:05:14 +0530

    update operation CRUD added

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -39,7 +39,7 @@ const getContact = asyncHandler(async (req, res) => {
     if (!contact) {
         res.status(404);
         throw new Error("Contact not found !");
-    } 
+    }
     res.status(200).json(contact);
 });
 
@@ -47,7 +47,20 @@ const getContact = asyncHandler(async (req, res) => {
 //@routes PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update contact for ${req.params.id}` });
+    const contact = await Contact.findById(req.params.id);
+
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found !");
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true },
+    );
+
+    res.status(200).json(updatedContact);
 });
 
 //@desc Delete contacts
