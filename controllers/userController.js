e4e5d420b06825e89953c7476781f4ca7e5ae3d7
AdishtJaglan <adisht7524@gmail.com>
2024-01-04 04:44:22 +0530

    access token added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //@desc Register a user
 //@routes POST /api/users/register
@@ -47,7 +48,31 @@ const registerUser = asyncHandler(async (req, res) => {
 //@routes POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Login user." });
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id,
+            },
+        }, process.env.ACCESS_TOKEN_SECRET,
+            { expirenIn: "1min" },
+        );
+
+        res.status(200).json({ accessToken });
+    } else {
+        res.status(401);
+        throw new Error("Email or password is invalid!");
+    }
 });
 
 //@desc Current user info
